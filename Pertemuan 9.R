9bebbe0eb5fa7c4268ed373a2ff74697698a816a
Nasywa Sofita <nasywasofita03@gmail.com>
2026-05-12 21:04:19 +0700

    data-summary-sum-sd-plot-cor

--- Pertemuan 9.R
+++ Pertemuan 9.R
@@ -2,5 +2,6 @@ data <- mtcars
 summary("mtcars")
 sum(mtcars$mpg)
 sd(mtcars$mpg)
+plot(mtcars$mpg, mtcars$hp, main="Hubungan MPG dan Horsepower")
 #Melihat korelasi antara konsumsi bensin (mpg) dan berat mobil (wt)
 cor(mtcars$mpg, mtcars$wt)
